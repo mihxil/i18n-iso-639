46395e1db12b378958cdbab1faa10824cf717004
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-02-11 22:59:34 +0100

    javadoc.

--- src/main/java/org/meeuw/i18n/languages/RetirementReason.java
+++ src/main/java/org/meeuw/i18n/languages/RetirementReason.java
@@ -1,12 +1,37 @@
 package org.meeuw.i18n.languages;
 
+/**
+ * The 'reason' for retirement of a code.
+ * @see RetiredLanguageCode
+ */
 public enum RetirementReason {
 
-    C ("change"),
-    D ("duplicate"),
-    N ("non-existent"),
-    S ("split"),
-    M ("merge");
+    /**
+     * 'change' The code has just changed. E.g. fri -> fry
+     */
+    C("change"),
+
+    /**
+     * 'duplicate'. This code is a duplicate of another code. E.g. bgh was a duplicate of bbh
+     */
+    D("duplicate"),
+
+    /**
+     * 'non-existent'. It was established that the language probably never existed.
+     * There is no replacement.
+     */
+    N("non-existent"),
+
+    /**
+     * 'split'. The language is now considered to be two or more languages.
+     * This cannot be automatically converted to the new code (there are more).
+     */
+    S("split"),
+
+     /**
+     * 'merge'. The language is not considered a separate language anymore, and its code is merged with another. E.g. the Souletin dialect of Basque is now considered to be just Basque ('eus').
+     */
+    M("merge");
 
     private final String string;
 
