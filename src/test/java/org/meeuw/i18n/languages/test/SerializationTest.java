99410de15d8f5fa3a3ac1a5310bf1250d7989cf4
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-02-21 22:20:08 +0100

    Testing.

--- src/test/java/org/meeuw/i18n/languages/test/SerializationTest.java
+++ src/test/java/org/meeuw/i18n/languages/test/SerializationTest.java
@@ -1,7 +1,8 @@
 package org.meeuw.i18n.languages.test;
 
-import java.io.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.xml.bind.JAXB;
+import java.io.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.meeuw.i18n.languages.LanguageCode;
@@ -17,9 +18,31 @@ public class SerializationTest {
         StringWriter writer = new StringWriter();
         JAXB.marshal(
             new A(LanguageCode.getByCode("nld").orElse(null)), writer);
+        System.out.println(writer.toString());
         A a = JAXB.unmarshal(new StringReader(writer.toString()), A.class);
         
+
+        
         assertThat(a.languageCode).isSameAs(languageCode("nld"));
+    }
+    
+    
+     /**
+     * Test serializing/deserializing to XML
+     */
+    @Test
+    public void json() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        //objectMapper.registerModule(new org.meeuw.i18n.languages.jackson.LanguageModule(true));
+        
+        String s = objectMapper.writeValueAsString(new A(LanguageCode.getByCode("nld").orElseThrow()));
+        System.out.println(s);
+        
+        assertThat(s).isEqualTo("{\"languageCode\":\"nl\"}");
+        
+        A rounded = objectMapper.readValue(s, A.class);
+        assertThat(rounded.languageCode.getCode()).isEqualTo("nl");
+         
     }
     
     
