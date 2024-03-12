63fefa81f75419a67cd4422dfed086ceb89c528a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-03-12 14:23:34 +0100

    Missing requires

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -20,6 +20,7 @@ module org.meeuw.i18n.languages {
     
     // jackson json binding (optional annotation)
     requires static com.fasterxml.jackson.databind;
+    requires static com.fasterxml.jackson.annotation;
     
     // but if used, these are needed too
     opens org.meeuw.i18n.languages to com.fasterxml.jackson.databind;
