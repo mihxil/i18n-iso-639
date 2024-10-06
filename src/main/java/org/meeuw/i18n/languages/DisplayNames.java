20be08d8b29f2a898622d418af3e629867ee5454
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2024-10-07 00:24:44 +0200

    javadoc.

--- src/main/java/org/meeuw/i18n/languages/DisplayNames.java
+++ src/main/java/org/meeuw/i18n/languages/DisplayNames.java
@@ -2,6 +2,12 @@ package org.meeuw.i18n.languages;
 
 import java.util.stream.Stream;
 
+/**
+ * All {@link ISO_639_Code#refName()} as a {@link java.util.ResourceBundle}.
+ * The default tab-files of ISO-636 provide the names of all language in english.
+ * This is the fallback for if a {@link java.util.PropertyResourceBundle} for given {@link java.util.Locale} is missing.
+ * @since 3.5
+ */
 public class DisplayNames extends java.util.ListResourceBundle {
     @Override
     protected Object[][] getContents() {
