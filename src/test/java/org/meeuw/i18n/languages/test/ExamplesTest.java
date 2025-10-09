c0f5b3d3ea034cb84c8d301a96b6901799a882a9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-10-09 13:16:07 +0200

    Made all example code actual code.

--- src/test/java/org/meeuw/i18n/languages/test/ExamplesTest.java
+++ src/test/java/org/meeuw/i18n/languages/test/ExamplesTest.java
@@ -4,7 +4,11 @@ import java.util.Locale;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+
+// tag::import[]
 import org.meeuw.i18n.languages.*;
+
+// end::import[]
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,27 +16,29 @@ public class ExamplesTest {
 
     @Test
     public void examples() {
-        // get a language by its code;
 
-        Optional<LanguageCode> optional = ISO_639.getByPart3("nld");
-        LanguageCode nl1 = LanguageCode.languageCode("nl");
+        // tag::dutch[]
+        // a language code can be obtained via static method
+        LanguageCode nl = LanguageCode.languageCode("nl");
         // For major language having a ISO-639-1 code there are enum values available
-        LanguageCode nl2 = ISO_639_1_Code.nl;
+        assertThat(nl).isSameAs(ISO_639_1_Code.nl);
 
-        // iso639 supports LanguageFamily and Language.
-        LanguageCode nl3 = ISO_639.iso639("nl", LanguageCode.class);
-        LanguageFamilyCode ger1 = ISO_639.iso639("gem", LanguageFamilyCode.class);
-        LanguageFamilyCode ger2 = LanguageFamilyCode.gem;
+        // other parts of the standards work too
+        assertThat(nl).isSameAs(LanguageCode.languageCode("dut")); // ISO-639-2/B code
+        assertThat(nl).isSameAs(LanguageCode.languageCode("nld")); // ISO-639-3 / ISO-639-2/T code
 
-        assertThat(nl1).isSameAs(nl2);
-        assertThat(nl2).isSameAs(nl3);
+        // use it in some way
+        assertThat(nl.nameRecord(Locale.US).inverted()).isEqualTo("Dutch");
 
-        // show its 'inverted' name
-        System.out.println(nl1.nameRecord(Locale.US).inverted());
+        // end::dutch[]
 
-        // get a language family
-        Optional<LanguageFamilyCode> family = ISO_639.getByPart5("gem");
-        assertThat(family.get()).isSameAs(ger1);
+        // tag::germanic[]
+        LanguageFamilyCode germanic = (LanguageFamilyCode) ISO_639.iso639("gem");
+        assertThat(germanic).isSameAs(LanguageFamilyCode.gem);
+        // there are not very many language families, so they are all available as enum values
+        assertThat(germanic).isSameAs(LanguageFamilyCode.valueOf("gem"));
+        // end::germanic[]
+
 
         // get by any code
         Optional<ISO_639_Code> byCode = ISO_639.get("nl");
@@ -41,5 +47,33 @@ public class ExamplesTest {
         ISO_639.streamByNames().forEach(e -> {
             System.out.println(e.getKey() + " " + e.getValue());
         });
+
+        // tag::krim[]
+
+        // the 'krim' dialect (Sierra Leone) officially merged into 'bmf' (Bom-Kim) in 2017
+
+        assertThat(ISO_639.getByPart3("krm").get().getCode()).isEqualTo("bmf");
+
+        // end::krim[]
+
+
+        // tag::fallback[]
+        // Our partner uses the pseudo ISO-639-1 code 'XX' for 'no language'
+        //  fall back to a proper Part 3 code.
+        try {
+            LanguageCode.registerFallback("XX", LanguageCode.languageCode("zxx"));
+            assertThat(ISO_639.iso639("XX").code()).isEqualTo("zxx");
+        } finally {
+            LanguageCode.resetFallBacks();
+        }
+        // end::fallback[]
+
+
+        // tag::dse[]
+
+        assertThat(LanguageCode.languageCode("dse").getDisplayName(new Locale("nl"))).isEqualTo("Nederlandse Gebarentaal");
+
+        // end::dse[]
+
     }
 }
