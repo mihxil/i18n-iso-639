b3b711e39ae73a1a9218dcec2d5c47c58891db7d
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-01-11 15:29:26 +0100

    simple demo.

--- docs/try.js
+++ docs/try.js
@@ -23,10 +23,9 @@ window.setup = async function setup() {
                 })
 
                 const version = "4.2-SNAPSHOT";
-                const pref = document.location.pathname.startsWith("/118n-iso-639/") ?
+                const prefix = document.location.pathname.startsWith("/118n-iso-639/") ?
                     "/app/i18n-iso-639/resources/" :
                     "/app/resources/";
-                const prefix = "/app/";
                 const classpath = `${prefix}i18n-iso-639-${version}.jar`;
                 console.log("Classpath:", classpath);
                 const cj = await cheerpjRunLibrary(classpath);
